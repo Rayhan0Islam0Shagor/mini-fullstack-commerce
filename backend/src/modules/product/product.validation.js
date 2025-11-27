6a21cdf2030bab3d938373999bbe8d2a0264e58f
Rayhan Islam Shagor <raihanislamshagor2@gmail.com>
2025-11-27 22:39:27 +0600

    added product validation with zod

---
+++ backend/src/modules/product/product.validation.js
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const createProductValidation = z.object({
+  title: z.string().min(3).max(100),
+  description: z.string().min(3).max(1000),
+  price: z.number().min(0),
+  category: z.string().min(3).max(100),
+  image: z.string().min(3).max(100),
+  rating: z.number().min(0).max(5),
+  stock: z.number().min(0),
+});
+
+export const updateProductValidation = z
+  .object({
+    id: z.number().min(1),
+    title: z.string().min(3).max(100),
+    description: z.string().min(3).max(1000),
+    price: z.number().min(0),
+    category: z.string().min(3).max(100),
+    image: z.string().min(3).max(100),
+    rating: z.number().min(0).max(5),
+    stock: z.number().min(0),
+  })
+  .partial();
+
+export const ProductValidation = {
+  createProductValidation,
+  updateProductValidation,
+};
